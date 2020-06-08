ba84db05e1eb341dab5da5d38b58110ccc7bdfd0
Patrik Grman <pato.grman@gmail.com>
2020-06-08 11:55:49 +0200

    Add and fix a matlab grammar from the ANTLR grammars repository

--- matlab2python/src/test/java/sk/uniba/grman19/GrammarTest.java
+++ matlab2python/src/test/java/sk/uniba/grman19/GrammarTest.java
@@ -1,13 +1,30 @@
 package sk.uniba.grman19;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
 import org.junit.jupiter.api.Test;
 
 public class GrammarTest {
-	
 	@Test
-	public void failedTest() {
-		fail("FAILED");
+	public void testSimpleProgram() {
+		//parse as translation_unit
+		String content=
+				"a=0;\n" +
+				"for i = 1:10\n" +
+				"a = a+i;\n" +
+				"end\n" +
+				"fprintf('a je %d', a);\n";
+		
+		MatlabLexer lexer=new MatlabLexer(CharStreams.fromString(content));
+		
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		
+		MatlabParser parser=new MatlabParser(tokens);
+		
+		parser.translation_unit().getText();
+		
+		assertEquals(0,parser.getNumberOfSyntaxErrors());
 	}
 }
